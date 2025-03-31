3be34f37694fcea99d6f419f39915803cbd7ac2d
Vishalpatil0111 <vp4137841@gmail.com>
2025-03-31 15:37:38 +0530

    Hero page layout

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -1,10 +1,21 @@
+import { div } from 'framer-motion/client'
 import React from 'react'
+import { FaStar } from 'react-icons/fa6'
 
 function Hero() {
   return (
-    <div className="w-full  flex mt-30 justify-center ">
-        <h2 className='text-2xl font-medium brightness-100 text-[#f2e1c0] tracking-wider'>You're always Welcome</h2>
+   <div>
+    
+     <div className="w-full p-5 px-10 flex mt-30 flex-col items-center justify-center ">
+      <h2 className='text-2xl font-medium brightness-100 text-[#f2e1c0] tracking-widest'>You're always Welcome</h2>
+
+      <div className='w-full h-fit  mt-5 justify-center items-center flex flex-col'>
+        <h1 className='text-[9vw] font-bold text-[#f2e1c0] tracking-wide opacity-90'>Fueling Your Day</h1>
+        <h4 className=' text-4xl font-bold text-[#f2e1c0] tracking-wide opacity-90  bg-amber-800'>With BrewBella !!</h4>
+      </div>
+
     </div>
+   </div>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Hero from '../components/Home/Hero'
-import Navbar from '../components/Navbar'
+
 
 
 function Home() {
   return (
-    <div className='w-full h-screen absolute brightness-50 bg-cover bg-center bg-[url(/images/herobg.jpg)]'>
-     
+    <div className='w-full h-screen absolute opacity-30 bg-cover bg-center bg-[url(/images/herobg.jpg)]'>
+     <Hero/>
       
     </div>
   )
